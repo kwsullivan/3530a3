1d26b5091c1b3c822cae080f15a47ebcb872fe98
arkaydias <kevin.sullivan016@gmail.com>
2018-11-18 19:40:21 -0500

    new files

---
+++ createTables.sql
@@ -0,0 +1,67 @@
+CREATE TABLE university (
+    name    VARCHAR,
+    uid     VARCHAR NOT NULL,
+    city    VARCHAR,
+    state   VARCHAR,
+    url     VARCHAR,
+    PRIMARY KEY (uid)
+);
+
+CREATE TABLE degree (
+    name    VARCHAR,
+    type    VARCHAR,
+    did     VARCHAR NOT NULL,
+    PRIMARY KEY (did)
+);
+
+CREATE TABLE country (
+    name    VARCHAR,
+    cid     VARCHAR NOT NULL,
+    PRIMARY KEY (cid)
+);
+
+CREATE TABLE degree_offered (
+    uid     VARCHAR NOT NULL,
+    did     VARCHAR NOT NULL,
+    PRIMARY KEY (uid, did),
+    FOREIGN KEY (uid) REFERENCES university (uid),
+    FOREIGN KEY (did) REFERENCES degree (did)
+);
+
+CREATE TABLE agents (
+    fname   VARCHAR,
+    lname   VARCHAR,
+    aid     VARCHAR,
+    phone   VARCHAR,
+    email   VARCHAR,
+    commission  VARCHAR,
+    resides_in_city VARCHAR,
+    cid     VARCHAR,
+    uid     VARCHAR,
+    PRIMARY KEY (aid),
+    FOREIGN KEY (uid) REFERENCES university (uid),
+    FOREIGN KEY (cid) REFERENCES country (cid)
+);
+
+CREATE TABLE recruits_from (
+    uid             VARCHAR NOT NULL,
+    cid             VARCHAR NOT NULL,
+    numstudents     VARCHAR,
+    PRIMARY KEY (uid, cid),
+    FOREIGN KEY (uid) REFERENCES university (uid),
+    FOREIGN KEY (cid) REFERENCES country (cid)
+);
+
+-- Copy CSV files into corresponding tables
+
+\copy university FROM '/tables/university.csv' WITH (FORMAT csv);
+
+\copy degree FROM '/tables/degree.csv' WITH (FORMAT csv);
+
+\copy country FROM '/tables/country.csv' WITH (FORMAT csv);
+
+\copy degree_offered FROM '/tables/degree_offered.csv' WITH (FORMAT csv);
+
+\copy agents FROM '/tables/agents.csv' WITH (FORMAT csv);
+
+\copy recruits_from FROM '/tables/recruits_from.csv' WITH (FORMAT csv);
